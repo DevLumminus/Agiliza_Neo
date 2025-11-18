a04d3092b0d11ab18f549512d3e7fd6cb129aabf
Lumminus <contaprojetos24@gmail.com>
2025-11-18 12:26:36 -0300

    Nav Rodapé

--- app/src/main/java/com/example/agilizaneo/MainActivity.kt
+++ app/src/main/java/com/example/agilizaneo/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DynamicColors.applyToActivitiesIfAvailable(application)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
